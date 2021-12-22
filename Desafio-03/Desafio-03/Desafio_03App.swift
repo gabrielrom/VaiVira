05cd688086fe15efb000a7855b09765b77c6b4d6
Marcos Felipe Rocha <marcos.rocha@vaivoa.com>
2021-12-22 10:56:22 -0300

    Criação do Projeto

---
+++ Desafio-03/Desafio-03/Desafio_03App.swift
@@ -0,0 +1,17 @@
+//
+//  Desafio_03App.swift
+//  Desafio-03
+//
+//  Created by MARCOS FELIPE SOARES ROCHA on 22/12/21.
+//
+
+import SwiftUI
+
+@main
+struct Desafio_03App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
